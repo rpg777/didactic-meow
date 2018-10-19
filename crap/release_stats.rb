cd136003b7dee7a90869001c7fc417fc4b3d85c6
Rob Guglielmetti <rob.guglielmetti@gmail.com>
2018-10-19 16:32:38 -0600

    reformat

--- crap/release_stats.rb
+++ crap/release_stats.rb
@@ -8,8 +8,6 @@
 require 'json'
 require 'open3'
 
-tempdata = "z_release_tempdata"
-
 stdout,stderr,status = Open3.capture3("wget -O - https://api.github.com/repos/NREL/Radiance/releases")
 
 STDERR.puts stderr
@@ -17,18 +15,19 @@ if status.success?
 
     releases = JSON.parse(stdout)
     puts "#{releases.size} releases:"
-    releases.each do |rel|
+    releases.reverse.each do |rel|
 
         dl = 0
 
-        puts "Release: #{rel['name']} (published #{rel['published_at']})"
+        puts "Release: #{rel['name']}"
+        puts "Published: #{rel['published_at']}"
         puts "Downloads (asset name, download count):"
         rel['assets'].each do |a|
             dl += a['download_count']
-            puts "\t#{a['name']}, #{a['download_count']}"
+            puts "-- #{a['name']}, #{a['download_count']}"
         end
 
-        puts "\tTotal downloads: #{dl}"
+        puts "Total downloads: #{dl}"
         puts
 
     end
